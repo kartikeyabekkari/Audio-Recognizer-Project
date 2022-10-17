0143fa549bf501278157a9db202cb800dec89530
kartikeyabekkari <83190693+kartikeyabekkari@users.noreply.github.com>
2022-10-16 19:20:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song1 = "";
+song2 = "";
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600,500);
+}
+
+function preload()
+{
+    song1 = loadSound("HarryPotter.mp3");
+    song2 = loadSound("thunder.mp3");
+}
+
+function play()
+{
+    song.play();
+}
